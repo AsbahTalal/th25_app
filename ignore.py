8dd732f73c43074c18dc9f6198a6cb390068e4e8
Han Nguyen <hgnguyen07@icloud.com>
2025-10-18 14:27:04 -0500

    yo

--- ignore.py
+++ ignore.py
@@ -5,12 +5,21 @@ from sys import exit
 width = 400
 height = 500
 
+#images
+#bg image
+bg_img = pg.image.load()
+
+
 pg.init()
-window = pg.display.setmode((width,height))
+info = pg.display.Info()
+window = pg.display.set_mode((info.current_w,info.current_h))
 pg.display.set_caption("Temp")
+clock = pg.time.Clock()
 
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
             exit()
+    pg.display.update()
+    clock.tick(60)
