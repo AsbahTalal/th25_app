78e321db999c194fe363e76ce274787eb1f56fc6
Han Nguyen <hgnguyen07@icloud.com>
2025-10-18 13:18:38 -0500

    yo

--- ignore.py
+++ ignore.py
@@ -1,13 +1,22 @@
+import pygame as pg
+import sys
+pg.init()
 
-from kivy.app import App
-from kivy.uix.gridLayout
+class Game:
+    def __init__(self):
+        self.width = 1920
+        self.height = 1080
+        self.win=pg.display.set_mode((self.width, self.height))
+        self.bg_img=pg.image.load("1.png").convert()
+        self.gameLoop()
 
-mylist = []
-for i in range(5):
-    mylist.append(i ** 2)
-print(mylist[-3:])
+    def gameLoop(self):
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    sys.exit()
+            self.win.blit(self.bg_img,(0,0))
+            pg.display.update()
 
-#changes 
-
-i=6
-j = 0
+game = Game()
