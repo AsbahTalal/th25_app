061b26b63817abcbe80e5295639ba47e94c6d399
kendrabman <kendrabman@gmail.com>
2025-10-18 19:21:15 -0500

    yo

--- start.py
+++ start.py
@@ -89,16 +89,31 @@ if __name__ == "__main__":
 obstacle_height = 200
 obstacles = []
 def create_obstacles():
+    #bench
     bench_img = pg.image.load('bench.png',)
     ogWidth, ogHeight = bench_img.get_size()
     obstacle_width = int(ogHeight * (ogWidth / ogHeight))
     bench_img = pg.transform.scale((obstacle_height,obstacle_width))
     obstacles.append(bench_img)
+    #duck
     duck_img = pg.image.load('duck.png',)
     ogWidth, ogHeight = duck_img.get_size()
     obstacle_width = int(ogHeight * (ogWidth / ogHeight))
     duck_img = pg.transform.scale(obstacle_height)
-    obstacles.append(bench_img)
+    obstacles.append(duck_img)
+    #scooter
+    scooter_img = pg.image.load('scooter.png',)
+    ogWidth, ogHeight = scooter_img.get_size()
+    obstacle_width = int(ogHeight * (ogWidth / ogHeight))
+    scooter_img = pg.transform.scale(obstacle_height)
+    obstacles.append(scooter_img)
+    #person
+    person1_img = pg.image.load('person1.png',)
+    ogWidth, ogHeight = person1_img.get_size()
+    obstacle_width = int(ogHeight * (ogWidth / ogHeight))
+    person1_img = pg.transform.scale(obstacle_height)
+    obstacles.append(person1_img)
+
 
 obstacle_x = WIDTH #to be defined
 obstacle_y = 0
