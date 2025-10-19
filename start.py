398b5f9165a04fd2d6ab1194b79d33ba1e3eabc1
kendrabman <kendrabman@gmail.com>
2025-10-18 19:19:01 -0500

    change file for obstacles

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame as pg
 import sys
 import time
 
@@ -84,3 +84,29 @@ def main_game():
 if __name__ == "__main__":
     start_sequence()
     main_game()
+
+
+obstacle_height = 200
+obstacles = []
+def create_obstacles():
+    bench_img = pg.image.load('bench.png',)
+    ogWidth, ogHeight = bench_img.get_size()
+    obstacle_width = int(ogHeight * (ogWidth / ogHeight))
+    bench_img = pg.transform.scale((obstacle_height,obstacle_width))
+    obstacles.append(bench_img)
+    duck_img = pg.image.load('duck.png',)
+    ogWidth, ogHeight = duck_img.get_size()
+    obstacle_width = int(ogHeight * (ogWidth / ogHeight))
+    duck_img = pg.transform.scale(obstacle_height)
+    obstacles.append(bench_img)
+
+obstacle_x = WIDTH #to be defined
+obstacle_y = 0
+obstacle_width = 0 #to be defined
+obstacle_height = 0 #to be defined
+
+class obstacle(pg.Rect):
+    def __init__(self, img):
+        pg.Rect.__init__(self, obstacle_x, obstacle_y, obstacle_width, obstacle_height)
+        self.img = img
+        self.passed = False
